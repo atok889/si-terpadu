84ab2fd2003fa8ce29d1f05560fe01f214e9aa7a
kurtz.slide <kurtz.slide@3a59f6d2-30a1-e0ef-d3eb-c9a0e4efb690>
2010-09-25 01:42:21 +0000

    RerataLamaStudiWnd

---
+++ SIA-Terpadu/src/java/org/sadhar/sia/terpadu/reratalamastudi/RerataLamaStudiWnd.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.sadhar.sia.terpadu.reratalamastudi;
+
+/**
+ *
+ * @author kurtz.slide
+ */
+public class RerataLamaStudiWnd {
+
+}
