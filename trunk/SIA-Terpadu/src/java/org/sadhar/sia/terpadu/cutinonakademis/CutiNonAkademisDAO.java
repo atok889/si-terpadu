4cfb463f364b46b92c70ac6c430e4c4de43be310
jasoet87 <jasoet87@3a59f6d2-30a1-e0ef-d3eb-c9a0e4efb690>
2010-11-12 04:21:03 +0000

    CutiNonAkademis

---
+++ trunk/SIA-Terpadu/src/java/org/sadhar/sia/terpadu/cutinonakademis/CutiNonAkademisDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sadhar.sia.terpadu.cutinonakademis;
+
+import java.util.List;
+
+/**
+ *
+ * @author jasoet
+ */
+public interface CutiNonAkademisDAO {
+
+    public List<CutiNonAkademis> gets(String kodeUnitKerja) throws Exception;
+}
