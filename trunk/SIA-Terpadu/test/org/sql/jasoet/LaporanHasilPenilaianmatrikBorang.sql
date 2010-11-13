cebf285fd8925652b7ec05366379da45421254de
jasoet87 <jasoet87@3a59f6d2-30a1-e0ef-d3eb-c9a0e4efb690>
2010-11-13 05:40:06 +0000

    Total Asset

--- trunk/SIA-Terpadu/test/org/sql/jasoet/LaporanHasilPenilaianmatrikBorang.sql
+++ trunk/SIA-Terpadu/test/org/sql/jasoet/LaporanHasilPenilaianmatrikBorang.sql
@@ -1,29 +1,34 @@
 --Perbandingan pra dan pasca monev
 --parameter unit kerja atau prodi..
-SELECT imovrka.kodeUnit,
-       imovrka.noIsian,
-       imovrka.isianMonevinRKA as NamaKomponen,
-       imovrka.skorPraMonev,
-       imovrka.skorPascaMonev
-  FROM mutu.isianMonevInRKA imovrka
- WHERE imovrka.kodeUnit LIKE '%1605%'
+SELECT
+        imovrka.tahun,
+        imovrka.kodeUnit,
+        imovrka.noIsian,
+        imovrka.isianMonevinRKA,
+        IF(imovrka.skorPascaMonev <=> null,0.0,imovrka.skorPascaMonev) AS skorPascaMonev,
+        IF(imovrka.skorPraMonev <=> null,0.0,imovrka.skorPraMonev) AS skorPraMonev
+FROM mutu.isianmonevinrka imovrka
+WHERE imovrka.kodeUnit LIKE '%1605%';
 
 --Perbandingan total  skor dari tahun ke tahun dg batas minimum 2005, 
 --parameter prodi saja..
-SELECT imovrka.tahun,
-      imovrka.kodeUnit,
-       imovrka.noIsian,
-       imovrka.isianMonevinRKA,
-       imovrka.skorPascaMonev
-  FROM mutu.isianMonevInRKA imovrka
-
-WHERE imovrka.tahun BETWEEN '2009' AND '2010'
+SELECT
+        imovrka.tahun,
+        imovrka.kodeUnit,
+        imovrka.noIsian,
+        imovrka.isianMonevinRKA,
+        IF(imovrka.skorPascaMonev <=> null,0.0,imovrka.skorPascaMonev) AS skorPascaMonev,
+        IF(imovrka.skorPraMonev <=> null,0.0,imovrka.skorPraMonev) AS skorPraMonev
+FROM mutu.isianmonevinrka imovrka
+WHERE imovrka.tahun BETWEEN '2009' AND '2010';
 
 --Perbandingan total skor antar unit kerja dan prodi pada tahun terakhir
-SELECT imovrka.kodeUnit,
-       imovrka.tahun
-       imovrka.noIsian,
-       imovrka.isianMonevinRKA,
-       imovrka.skorPascaMonev
-  FROM mutu.isianMonevInRKA imovrka
- WHERE imovrka.kodeUnit LIKE '%1605%' AND imovrka.tahun=2010
+SELECT
+        imovrka.tahun,
+        imovrka.kodeUnit,
+        imovrka.noIsian,
+        imovrka.isianMonevinRKA,
+        IF(imovrka.skorPascaMonev <=> null,0.0,imovrka.skorPascaMonev) AS skorPascaMonev,
+        IF(imovrka.skorPraMonev <=> null,0.0,imovrka.skorPraMonev) AS skorPraMonev
+FROM mutu.isianmonevinrka imovrka
+WHERE imovrka.kodeUnit LIKE '%1605%' AND imovrka.tahun='2010';
