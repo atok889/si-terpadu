1cb3e0d340f534088641026b18bb5bf8b5151241
jasoet87 <jasoet87@3a59f6d2-30a1-e0ef-d3eb-c9a0e4efb690>
2010-11-14 07:07:22 +0000

    NEPDosen

--- trunk/SIA-Terpadu/src/java/org/sadhar/sia/terpadu/nepdosen/NEPDosenDAOImpl.java
+++ trunk/SIA-Terpadu/src/java/org/sadhar/sia/terpadu/nepdosen/NEPDosenDAOImpl.java
@@ -37,7 +37,40 @@ public class NEPDosenDAOImpl implements NEPDosenDAO {
                 + "       ON (pegawai.NPP = unit_peg.NPP)) "
                 + "    INNER JOIN evaluasi.resume_kategori resume_kategori "
                 + "     ON (resume_kategori.npp = pegawai.NPP) "
-                + "    WHERE resume_kategori.ta LIKE '2008%' AND unit_peg.Kd_unit LIKE '%1609%' "
+                + "    WHERE resume_kategori.ta LIKE '"+tahun+"%' AND unit_peg.Kd_unit LIKE '%" + kodeUnit + "%' "
+                + "  GROUP BY resume_kategori.npp";
+        List<NEPDosen> list = new ArrayList<NEPDosen>();
+        List<Map> rows = ClassConnection.getJdbc().queryForList(sql);
+        for (Map m : rows) {
+            NEPDosen nepdosen = new NEPDosen();
+            nepdosen.setKodeUnit(ClassAntiNull.AntiNullString(m.get("kodeUnit")));
+            nepdosen.setNamaPegawai(ClassAntiNull.AntiNullString(m.get("namaPegawai")));
+            nepdosen.setNamaUnit(ClassAntiNull.AntiNullString(m.get("namaUnit")));
+            nepdosen.setKodeUnit(ClassAntiNull.AntiNullString(m.get("kodeUnit")));
+            nepdosen.setRataRata(ClassAntiNull.AntiNullDouble(m.get("rataRata")));
+            nepdosen.setTahunAjaran(tahun);
+            list.add(nepdosen);
+        }
+        return list;
+    }
+
+    public List<NEPDosen> getAll() throws Exception {
+        String tahun = "2008";
+//         String tahun = Calendar.getInstance().get(Calendar.YEAR) + "";
+        String sql = "SELECT resume_kategori.npp as npp, "
+                + "    pegawai.Nama_peg as namaPegawai, "
+                + "     unit_peg.Kd_unit as kodeUnit, "
+                + "    unkerja.Nama_unit_kerja as namaUnit, "
+                + "    AVG(resume_kategori.skor) AS rataRata, "
+                + "    resume_kategori.ta AS tahunAjaran "
+                + "  FROM ((kamus.unkerja unkerja "
+                + "        INNER JOIN kamus.unit_peg unit_peg "
+                + "         ON (unkerja.Kd_unit_kerja = unit_peg.Kd_unit)) "
+                + "   INNER JOIN personalia.pegawai pegawai "
+                + "       ON (pegawai.NPP = unit_peg.NPP)) "
+                + "    INNER JOIN evaluasi.resume_kategori resume_kategori "
+                + "     ON (resume_kategori.npp = pegawai.NPP) "
+                + "    WHERE resume_kategori.ta LIKE '"+tahun+"%' "
                 + "  GROUP BY resume_kategori.npp";
         List<NEPDosen> list = new ArrayList<NEPDosen>();
         List<Map> rows = ClassConnection.getJdbc().queryForList(sql);
---
+++ trunk/SIA-Terpadu/src/java/org/sadhar/sia/terpadu/nepdosen/NEPDosenWnd.java
@@ -0,0 +1,138 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sadhar.sia.terpadu.nepdosen;
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.jfree.chart.JFreeChart;
+import org.sadhar.sia.framework.ClassApplicationModule;
+import org.sadhar.sia.terpadu.ukprogramstudi.UKProgramStudi;
+import org.sadhar.sia.terpadu.ukprogramstudi.UKProgramStudiDAO;
+import org.sadhar.sia.terpadu.ukprogramstudi.UKProgramStudiDAOImpl;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zkex.zul.Jasperreport;
+import org.zkoss.zul.Button;
+import org.zkoss.zul.Combobox;
+import org.zkoss.zul.Comboitem;
+import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Listcell;
+import org.zkoss.zul.Listitem;
+import org.zkoss.zul.Messagebox;
+import org.zkoss.zul.Window;
+
+/**
+ *
+ * @author Deny Prasetyo
+ */
+public class NEPDosenWnd extends ClassApplicationModule {
+
+    Combobox cmbProgdi;
+    Jasperreport report;
+//    Image chartImg;
+    Combobox cmbExportType;
+    Button btnExport;
+    Listbox listb;
+    JFreeChart chart = null;
+    List<NEPDosen> datas;
+
+    public NEPDosenWnd() {
+    }
+
+    public void onCreate() throws Exception {
+        listb = (Listbox) getFellow("listb");
+        cmbProgdi = (Combobox) getFellow("cmbProgdi");
+        report = (Jasperreport) getFellow("report");
+        cmbExportType = (Combobox) getFellow("cmbExportType");
+        btnExport = (Button) getFellow("btnExport");
+        btnExport.setDisabled(true);
+        loadProgdi();
+        cmbProgdi.setSelectedIndex(0);
+        cmbExportType.setSelectedIndex(0);
+    }
+
+    private void loadProgdi() throws Exception {
+        try {
+            UKProgramStudiDAO dao = new UKProgramStudiDAOImpl();
+            List<UKProgramStudi> progdis = dao.gets();
+            cmbProgdi.getItems().clear();
+            Comboitem item = new Comboitem();
+            item.setValue(null);
+            item.setLabel("--Pilih Prodi--");
+            cmbProgdi.appendChild(item);
+            for (UKProgramStudi ps : progdis) {
+                Comboitem items = new Comboitem();
+                items.setValue(ps);
+                items.setLabel(ps.getKodeUnitKerja() + " " + ps.getNama());
+                cmbProgdi.appendChild(items);
+            }
+        } catch (Exception ex) {
+            Messagebox.show(ex.getMessage());
+        }
+    }
+
+    public void viewReport() throws Exception {
+        try {
+            listb.getItems().clear();
+            NEPDosenDAO dao = new NEPDosenDAOImpl();
+            UKProgramStudi prodiSelected = (UKProgramStudi) cmbProgdi.getSelectedItem().getValue();
+            if (prodiSelected == null) {
+                datas = dao.getAll();
+            } else {
+
+                datas = dao.getByKodeUnit(prodiSelected.getKodeUnitKerja());
+            }
+
+            DecimalFormat decimalFormat = new DecimalFormat("##0.00");
+            int i = 1;
+            for (NEPDosen o : datas) {
+                Listitem li = new Listitem();
+                Listcell cell = new Listcell("" + (i++));
+                li.appendChild(cell);
+                cell = new Listcell(o.getNamaPegawai());
+                li.appendChild(cell);
+                cell = new Listcell(decimalFormat.format(o.getRataRata()));
+                cell.setStyle("text-align:right");
+                li.appendChild(cell);
+                listb.appendChild(li);
+
+            }
+            btnExport.setDisabled(false);
+
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            Messagebox.show(ex.getMessage());
+        }
+    }
+
+    public void exportReport() throws Exception {
+        try {
+            JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(datas);
+            if (cmbExportType.getSelectedItem().getValue().toString().equals("pdf")) {
+                Window pdfPreviewWnd = (Window) Executions.createComponents("/zul/pdfpreview/PdfPreview.zul", null, null);
+                Jasperreport pdfReport = (Jasperreport) pdfPreviewWnd.getFellow("report");
+                pdfReport.setType(cmbExportType.getSelectedItem().getValue().toString());
+                pdfReport.setSrc("reports/nepdosen/NEPDosen.jasper");
+                Map parameters = new HashMap();
+//                parameters.put("chart", chart.createBufferedImage(500, 300));
+                pdfReport.setParameters(parameters);
+                pdfReport.setDatasource(dataSource);
+                pdfPreviewWnd.doModal();
+            } else {
+                report.setType(cmbExportType.getSelectedItem().getValue().toString());
+                report.setSrc("reports/nepdosen/NEPDosen.jasper");
+                Map parameters = new HashMap();
+//                parameters.put("chart", chart.createBufferedImage(500, 300, BufferedImage.TRANSLUCENT, null));
+                report.setParameters(parameters);
+                report.setDatasource(dataSource);
+            }
+        } catch (Exception ex) {
+            Messagebox.show(ex.getMessage());
+        }
+    }
+}
