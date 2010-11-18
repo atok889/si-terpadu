0a5184464ff127743653f5f966ab821c46fecf05
jasoet87 <jasoet87@3a59f6d2-30a1-e0ef-d3eb-c9a0e4efb690>
2010-11-18 05:22:28 +0000

    Pra Pasca Monev

--- SIA-Terpadu/src/java/org/sadhar/sia/terpadu/jumlahmahasiswado/JumlahMahasiswaDODAOImpl.java
+++ SIA-Terpadu/src/java/org/sadhar/sia/terpadu/jumlahmahasiswado/JumlahMahasiswaDODAOImpl.java
@@ -5,6 +5,7 @@
 package org.sadhar.sia.terpadu.jumlahmahasiswado;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -41,20 +42,32 @@ public class JumlahMahasiswaDODAOImpl implements JumlahMahasiwaDODAO {
     public CategoryDataset getDataset(ProgramStudi progdi, String tahun) throws Exception {
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
         String sql = "";
+
+
+        try {
+            int tahunI = Integer.parseInt(tahun);
+
+            if (tahunI < 2005 || tahunI > Calendar.getInstance().get(Calendar.YEAR)) {
+                tahun = Calendar.getInstance().get(Calendar.YEAR) + "";
+            }
+        } catch (NumberFormatException ex) {
+            tahun = "";
+        }
+
         if (progdi != null && tahun.isEmpty()) {
             DateTime dt = new DateTime(new Date());
             sql = "SELECT YEAR(tgl_sk_do) AS tahun,COUNT(nomor_mhs) AS jumlah "
                     + "FROM db_" + progdi.getKode() + ".do" + progdi.getKode() + " WHERE YEAR(tgl_sk_do) BETWEEN '" + (dt.getYear() - 5) + "' AND '" + dt.getYear() + "' GROUP BY YEAR(tgl_sk_do)";
 
             List<Map> rows = null;
-                try {
-                    rows = ClassConnection.getJdbc().queryForList(sql);
-                    for (Map m : rows) {
-                        dataset.addValue(Integer.valueOf(m.get("jumlah").toString()), m.get("tahun").toString(), progdi.getNama());
-                    }
-                } catch (DataAccessException dae) {
-                    System.out.println("Data invalid Silahkan perbaiki Validitas dan Integritas Data");
+            try {
+                rows = ClassConnection.getJdbc().queryForList(sql);
+                for (Map m : rows) {
+                    dataset.addValue(Integer.valueOf(m.get("jumlah").toString()), m.get("tahun").toString(), progdi.getNama());
                 }
+            } catch (DataAccessException dae) {
+                System.out.println("Data invalid Silahkan perbaiki Validitas dan Integritas Data");
+            }
 
         } else if (progdi != null && !tahun.isEmpty()) {
             sql = "SELECT YEAR(tgl_sk_do) AS tahun,COUNT(nomor_mhs) AS jumlah "
@@ -66,15 +79,12 @@ public class JumlahMahasiswaDODAOImpl implements JumlahMahasiwaDODAO {
                     dataset.addValue(Integer.valueOf(m.get("jumlah").toString()), m.get("tahun").toString(), progdi.getNama());
                 }
             } catch (DataAccessException dae) {
-              System.out.println("Data invalid Silahkan perbaiki Validitas dan Integritas Data");
+                System.out.println("Data invalid Silahkan perbaiki Validitas dan Integritas Data");
             }
 
         } else if (progdi == null && !tahun.isEmpty()) {
             List<ProgramStudi> progdis = new ArrayList<ProgramStudi>();
             progdis = getProgramStudi();
-            //for sampe
-//            progdis.add(new ProgramStudi("5013", "MEKATRONIKA (D3)"));
-//            progdis.add(new ProgramStudi("5314", "TEKNIK INFORMATIKA"));
 
             for (ProgramStudi ps : progdis) {
                 sql = "SELECT YEAR(tgl_sk_do) AS tahun,COUNT(nomor_mhs) AS jumlah "
@@ -94,9 +104,6 @@ public class JumlahMahasiswaDODAOImpl implements JumlahMahasiwaDODAO {
             DateTime dt = new DateTime(new Date());
             List<ProgramStudi> progdis = new ArrayList<ProgramStudi>();
             progdis = getProgramStudi();
-            //for sampe
-//            progdis.add(new ProgramStudi("5013", "MEKATRONIKA (D3)"));
-//            progdis.add(new ProgramStudi("5314", "TEKNIK INFORMATIKA"));
 
             for (ProgramStudi ps : progdis) {
                 sql = "SELECT YEAR(tgl_sk_do) AS tahun,COUNT(nomor_mhs) AS jumlah "
