2d373eb81ad82cfbaaddf90f4e64f39ec303a935
jasoet87 <jasoet87@3a59f6d2-30a1-e0ef-d3eb-c9a0e4efb690>
2010-11-18 04:33:23 +0000

    Pra Pasca Monev

---
+++ trunk/SIA-Terpadu/src/java/org/sadhar/sia/terpadu/matrikborang/PerbandinganTotalSkorTahunWnd.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.sadhar.sia.terpadu.matrikborang;
+
+/**
+ *
+ * @author jasoet
+ */
+public class PerbandinganTotalSkorTahunWnd {
+
+}
