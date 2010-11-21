1e5f2aa10e709e9430466a30516e0e52a476ce81
kurtz.slide@gmail.com <kurtz.slide@gmail.com@3a59f6d2-30a1-e0ef-d3eb-c9a0e4efb690>
2010-11-21 09:44:54 +0000

    per prodi tahun 2010

--- trunk/SIA-Terpadu/test/org/sql/jasoet/pendapatan_realisasi.sql
+++ trunk/SIA-Terpadu/test/org/sql/jasoet/pendapatan_realisasi.sql
@@ -1,3 +1,4 @@
+-- REALISASI PENDAPATAN dari PROGRAM STUDI TEKNIK INFORMATIKA tahun 2010--
 SELECT tg531420102.kd_tagih,
          tagihan.nama_tagih,
          SUM(tg531420102.jumlah) as realisasi,
@@ -17,3 +18,24 @@ SELECT tg531420101.kd_tagih,
    WHERE (tg531420101.kd_tagih != tg531420101.tgl_bayar)
          AND(tg531420101.tgl_bayar IS NOT NULL)
 GROUP BY tg531420101.kd_tagih
+-----------------------------------------------------------------------------------------------------------
+-----REALISASI PENDAPATAN dari PROGRAM STUDI TEKNIK MESIN tahun 2010-------------
+SELECT tg521420102.kd_tagih,
+         tagihan.nama_tagih,
+         SUM(tg521420102.jumlah) as realisasi,
+         tg521420102.tgl_bayar
+    FROM db_5214.tg521420102 tg521420102 INNER JOIN kamus.tagihan tagihan
+            ON (tg521420102.kd_tagih = tagihan.Kd_tagih)
+   WHERE (tg521420102.kd_tagih != tg521420102.tgl_bayar)
+         AND(tg521420102.tgl_bayar IS NOT NULL)
+GROUP BY tg521420102.kd_tagih
+UNION ALL
+SELECT tg521420101.kd_tagih,
+         tagihan.nama_tagih,
+         SUM(tg521420101.jumlah) as realisasi,
+         tg521420101.tgl_bayar
+    FROM db_5214.tg521420101 tg521420101 INNER JOIN kamus.tagihan tagihan
+            ON (tg521420101.kd_tagih = tagihan.Kd_tagih)
+   WHERE (tg521420101.kd_tagih != tg521420101.tgl_bayar)
+         AND(tg521420101.tgl_bayar IS NOT NULL)
+GROUP BY tg521420101.kd_tagih
