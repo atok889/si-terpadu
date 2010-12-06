4feafc75edf8811ca61135617f2c34b66d4a3e41
kurtz.slide@gmail.com <kurtz.slide@gmail.com@3a59f6d2-30a1-e0ef-d3eb-c9a0e4efb690>
2010-12-06 13:08:54 +0000

    revisi query sql

--- trunk/SIA-Terpadu/src/java/org/sadhar/sia/terpadu/cutinonakademis/CutiNonAkademisDAOImpl.java
+++ trunk/SIA-Terpadu/src/java/org/sadhar/sia/terpadu/cutinonakademis/CutiNonAkademisDAOImpl.java
@@ -26,7 +26,7 @@ public class CutiNonAkademisDAOImpl implements CutiNonAkademisDAO {
         if (kodeUnitKerja.isEmpty()) {
             whereUnitKerja = "";
         } else {
-            whereUnitKerja = "AND kamus.unkerja.`kd_unit_kerja` LIKE " + kodeUnitKerja;
+            whereUnitKerja = "AND kamus.unkerja.`kd_unit_kerja` = " + kodeUnitKerja;
 
         }
 
@@ -34,24 +34,42 @@ public class CutiNonAkademisDAOImpl implements CutiNonAkademisDAO {
         String tahun = cal.get(Calendar.YEAR) + "";
 
 //        String tahun = "2008";
-        String sql = "SELECT "
-                + " cuti" + tahun + ".kdPegawai AS kodePegawai,  "
-                + " pegawai.Nama_peg AS namaPegawai,  "
-                + " kamus.unkerja.nama_unit_kerja AS unitKerja, "
-                + " SUM(kamus.jns_cuti.jatah_cuti - `cuti" + tahun + "`.`lama_cuti`) AS sisaCuti, "
-                + " unit_peg.kd_unit, "
-                + " `cuti" + tahun + "`.`lama_cuti`, "
-                + " kamus.jns_cuti.nama_cuti "
-                + " FROM "
-                + " personalia.cuti" + tahun + " "
-                + " LEFT OUTER JOIN personalia.pegawai ON (cuti" + tahun + ".kdPegawai = pegawai.kdPegawai) "
-                + " LEFT OUTER JOIN personalia.unit_peg ON (cuti" + tahun + ".kdPegawai = unit_peg.kdPegawai) "
-                + " INNER JOIN kamus.jns_cuti ON (`cuti" + tahun + "`.`Jenis_cuti`=kamus.jns_cuti.Kd_cuti) "
-                + " INNER JOIN kamus.unkerja ON (`unit_peg`.`kd_unit`=kamus.unkerja.`kd_unit_kerja`) "
-                + " WHERE "
-                + " `pegawai`.`AdmEdu`LIKE 1 AND `cuti" + tahun + "`.`Jenis_cuti` LIKE 1 " + whereUnitKerja
-                + " GROUP BY "
-                + " cuti" + tahun + ".kdPegawai";
+//        String sql = "SELECT "
+//                + " cuti" + tahun + ".kdPegawai AS kodePegawai,  "
+//                + " pegawai.Nama_peg AS namaPegawai,  "
+//                + " kamus.unkerja.nama_unit_kerja AS unitKerja, "
+//                + " SUM(kamus.jns_cuti.jatah_cuti - `cuti" + tahun + "`.`lama_cuti`) AS sisaCuti, "
+//                + " unit_peg.kd_unit, "
+//                + " `cuti" + tahun + "`.`lama_cuti`, "
+//                + " kamus.jns_cuti.nama_cuti "
+//                + " FROM "
+//                + " personalia.cuti" + tahun + " "
+//                + " LEFT OUTER JOIN personalia.pegawai ON (cuti" + tahun + ".kdPegawai = pegawai.kdPegawai) "
+//                + " LEFT OUTER JOIN personalia.unit_peg ON (cuti" + tahun + ".kdPegawai = unit_peg.kdPegawai) "
+//                + " INNER JOIN kamus.jns_cuti ON (`cuti" + tahun + "`.`Jenis_cuti`=kamus.jns_cuti.Kd_cuti) "
+//                + " INNER JOIN kamus.unkerja ON (`unit_peg`.`kd_unit`=kamus.unkerja.`kd_unit_kerja`) "
+//                + " WHERE "
+//                + " `pegawai`.`AdmEdu`LIKE 1 AND `cuti" + tahun + "`.`Jenis_cuti` LIKE 1 " + whereUnitKerja
+//                + " GROUP BY "
+//                + " cuti" + tahun + ".kdPegawai";
+//
+        String sql =  "SELECT cutipegawai.NPP AS kodePegawai,"
+         + " pegawai.Nama_peg AS namaPegawai,"
+         + " jns_cuti.Nama_cuti AS nama_cuti,"
+         + " kamus.unkerja.nama_unit_kerja AS unitKerja,"
+         + " jns_cuti.jatah_cuti - SUM(cutipegawai.lama_cuti) AS sisaCuti,"
+          + " unit_peg.kd_unit,"
+         + " SUM(cutipegawai.lama_cuti) AS lama_cuti"
+           + " FROM personalia.cutiPegawai cutipegawai "
+         + " INNER JOIN personalia.pegawai pegawai ON (cutipegawai.NPP = pegawai.NPP)"
+         + " INNER JOIN kamus.jns_cuti jns_cuti  ON (jns_cuti.Kd_cuti = cutipegawai.Jenis_cuti)"
+         + " INNER JOIN personalia.unit_peg ON (cutipegawai.NPP = unit_peg.NPP)"
+         + " INNER JOIN kamus.unkerja  ON (unit_peg.kd_unit = kamus.unkerja.kd_unit_kerja)"
+         + " WHERE YEAR(cutipegawai.Tgl_Akhir_cuti) LIKE "+tahun+"" + whereUnitKerja
+         + " AND cutipegawai.Jenis_cuti LIKE 1 "
+         + " AND pegawai.AdmEdu LIKE 1 "
+            + " GROUP BY pegawai.Nama_peg";
+      
 
         List<CutiNonAkademis> data = new ArrayList<CutiNonAkademis>();
         List<Map> rows = ClassConnection.getJdbc().queryForList(sql);
