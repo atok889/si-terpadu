276bd66e05bd029696ba3b16b8568b1ea7395ffe
krissadewo <krissadewo@3a59f6d2-30a1-e0ef-d3eb-c9a0e4efb690>
2010-12-06 10:30:39 +0000

    Revisi

---
+++ SIA-Terpadu/src/java/org/sadhar/sia/terpadu/rasiodosenmahasiswa/berdasarkansemester/RasioDosenMahasiswaDAO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sadhar.sia.terpadu.rasiodosenmahasiswa.berdasarkansemester;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author kris
+ */
+public interface RasioDosenMahasiswaDAO {
+
+    public List<Map> getProdi();
+
+    public List<Map> getNamaDosen(String kodeProdi, String tahun, String semester);
+
+    public List<Map> getRasioDosenMahasiswa(String kodeProdi, String tahun, String semester);
+}
