3446232af7be24cd763cdc2dd4ce7c755459063b
jasoet87 <jasoet87@3a59f6d2-30a1-e0ef-d3eb-c9a0e4efb690>
2010-12-07 22:58:52 +0000

    Perbaikan Table Cuti Non Akademis

--- SIA-Terpadu/test/org/sql/jasoet/CutiNonAkademis.sql
+++ SIA-Terpadu/test/org/sql/jasoet/CutiNonAkademis.sql
@@ -1,38 +1,38 @@
 SELECT
-cuti2008.kdPegawai,
+cutiPegawai.kdPegawai,
 pegawai.Nama_peg,
 kamus.unkerja.nama_unit_kerja,
-SUM(kamus.jns_cuti.jatah_cuti - `cuti2008`.`lama_cuti`) AS SISA_CUTI_TAHUNAN,
+SUM(kamus.jns_cuti.jatah_cuti - `cutiPegawai`.`lama_cuti`) AS SISA_CUTI_TAHUNAN,
 unit_peg.kd_unit,
-`cuti2008`.`lama_cuti`,
+`cutiPegawai`.`lama_cuti`,
 kamus.jns_cuti.nama_cuti
 FROM
-personalia.cuti2008
-LEFT OUTER JOIN personalia.pegawai ON (cuti2008.kdPegawai = pegawai.kdPegawai)
-LEFT OUTER JOIN personalia.unit_peg ON (cuti2008.kdPegawai = unit_peg.kdPegawai)
-INNER JOIN kamus.jns_cuti ON (`cuti2008`.`Jenis_cuti`=kamus.jns_cuti.Kd_cuti)
+personalia.cutiPegawai
+LEFT OUTER JOIN personalia.pegawai ON (cutiPegawai.kdPegawai = pegawai.kdPegawai)
+LEFT OUTER JOIN personalia.unit_peg ON (cutiPegawai.kdPegawai = unit_peg.kdPegawai)
+INNER JOIN kamus.jns_cuti ON (`cutiPegawai`.`Jenis_cuti`=kamus.jns_cuti.Kd_cuti)
 INNER JOIN kamus.unkerja ON (`unit_peg`.`kd_unit`=kamus.unkerja.`kd_unit_kerja`)
 WHERE
-`pegawai`.`AdmEdu`LIKE 1 AND `cuti2008`.`Jenis_cuti` LIKE 1
+`pegawai`.`AdmEdu`LIKE 1 AND `cutiPegawai`.`Jenis_cuti` LIKE 1
 GROUP BY
-cuti2008.kdPegawai;
+cutiPegawai.kdPegawai;
 
 
 SELECT
-cuti2008.kdPegawai,
+cutiPegawai.kdPegawai,
 pegawai.Nama_peg,
 kamus.unkerja.nama_unit_kerja,
-SUM(kamus.jns_cuti.jatah_cuti - `cuti2008`.`lama_cuti`) AS SISA_CUTI_TAHUNAN,
+SUM(kamus.jns_cuti.jatah_cuti - `cutiPegawai`.`lama_cuti`) AS SISA_CUTI_TAHUNAN,
 unit_peg.kd_unit,
-`cuti2008`.`lama_cuti`,
+`cutiPegawai`.`lama_cuti`,
 kamus.jns_cuti.nama_cuti
 FROM
-personalia.cuti2008
-LEFT OUTER JOIN personalia.pegawai ON (cuti2008.kdPegawai = pegawai.kdPegawai)
-LEFT OUTER JOIN personalia.unit_peg ON (cuti2008.kdPegawai = unit_peg.kdPegawai)
-INNER JOIN kamus.jns_cuti ON (`cuti2008`.`Jenis_cuti`=kamus.jns_cuti.Kd_cuti)
+personalia.cutiPegawai
+LEFT OUTER JOIN personalia.pegawai ON (cutiPegawai.kdPegawai = pegawai.kdPegawai)
+LEFT OUTER JOIN personalia.unit_peg ON (cutiPegawai.kdPegawai = unit_peg.kdPegawai)
+INNER JOIN kamus.jns_cuti ON (`cutiPegawai`.`Jenis_cuti`=kamus.jns_cuti.Kd_cuti)
 INNER JOIN kamus.unkerja ON (`unit_peg`.`kd_unit`=kamus.unkerja.`kd_unit_kerja`)
 WHERE
-`pegawai`.`AdmEdu`LIKE 1 AND `cuti2008`.`Jenis_cuti` LIKE 1 AND kamus.unkerja.`kd_unit_kerja` LIKE 12000000
+`pegawai`.`AdmEdu`LIKE 1 AND `cutiPegawai`.`Jenis_cuti` LIKE 1 AND kamus.unkerja.`kd_unit_kerja` LIKE 12000000
 GROUP BY
-cuti2008.kdPegawai;
+cutiPegawai.kdPegawai;
