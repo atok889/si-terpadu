444bceebf9a1e25faec558a11d63e1f1ece4414d
krissadewo <krissadewo@3a59f6d2-30a1-e0ef-d3eb-c9a0e4efb690>
2010-12-15 03:48:49 +0000

    Revisi

--- trunk/SIA-Terpadu/src/java/org/sadhar/sia/terpadu/rasiodosenmahasiswa/berdasarkansemester/RasioDosenMahasiswaDAOImpl.java
+++ trunk/SIA-Terpadu/src/java/org/sadhar/sia/terpadu/rasiodosenmahasiswa/berdasarkansemester/RasioDosenMahasiswaDAOImpl.java
@@ -29,22 +29,22 @@ public class RasioDosenMahasiswaDAOImpl implements RasioDosenMahasiswaDAO {
     public List<Map> getRasioDosenMahasiswa(String kodeProdi, String tahun, String semester) {
         List<Map> results = new ArrayList<Map>();
         if (isTabelJwExist(kodeProdi, tahun, semester) && isTabelKrExist(kodeProdi, tahun, semester)) {
-            String sqlCreateView = "CREATE OR REPLACE VIEW kamus.rasioDosenMahasiswa(jumlah,kd_mtk,nama) AS " +
+            String sqlCreateView = "CREATE OR REPLACE VIEW tempo.rasioDosenMahasiswa(jumlah,kd_mtk,nama) AS " +
                     "SELECT COUNT(nomor_mhs) AS jumlah, krs.kd_mtk, 0 AS Nama_peg " +
                     " FROM db_" + kodeProdi + ".kr" + kodeProdi + tahun + semester + " krs  GROUP BY kd_mtk " +
                     " UNION " +
-                    " SELECT 0 as jumlah, jw.kd_mtk,  concat_ws(' ',pp.Gelar_depan,pp.Nama_peg,pp.Gelar_blk) as Nama_peg FROM personalia.pegawai pp " +
+                    " SELECT 0 as jumlah, jw.kd_mtk,  LTRIM(concat_ws(' ',pp.Gelar_depan,pp.Nama_peg,pp.Gelar_blk)) as Nama_peg FROM personalia.pegawai pp " +
                     " INNER JOIN kamus.stat_edu ks ON pp.AdmEdu = ks.Kd_stat_edu " +
                     " INNER JOIN db_" + kodeProdi + ".jw" + kodeProdi + tahun + semester + " jw ON pp.kdPegawai = jw.NPP " +
                     " WHERE ks.Kd_stat_edu='2'";
-           
+
             ClassConnection.getJdbc().execute(sqlCreateView);
 
             String sql = " SELECT kd_mtk,max(jumlah) AS jumlah, max(nama) AS nama " +
-                    " FROM kamus.rasioDosenMahasiswa GROUP BY kd_mtk";
+                    " FROM tempo.rasioDosenMahasiswa GROUP BY kd_mtk";
 
             results.addAll(ClassConnection.getJdbc().queryForList(sql));
-        }     
+        }
         return results;
     }
 
@@ -75,7 +75,7 @@ public class RasioDosenMahasiswaDAOImpl implements RasioDosenMahasiswaDAO {
     public List<Map> getNamaDosen(String kodeProdi, String tahun, String semester) {
         List<Map> results = new ArrayList<Map>();
         if (isTabelJwExist(kodeProdi, tahun, semester) && isTabelKrExist(kodeProdi, tahun, semester)) {
-            String sql = "SELECT DISTINCT(concat_ws(' ',pp.Gelar_depan,pp.Nama_peg,pp.Gelar_blk)) AS nama FROM personalia.pegawai pp " +
+            String sql = "SELECT DISTINCT LTRIM(concat_ws(' ',pp.Gelar_depan,pp.Nama_peg,pp.Gelar_blk)) AS nama FROM personalia.pegawai pp " +
                     " INNER JOIN kamus.stat_edu ks ON pp.AdmEdu=ks.Kd_stat_edu " +
                     " INNER JOIN db_" + kodeProdi + ".jw" + kodeProdi + tahun + semester + " jw ON pp.kdPegawai = jw.NPP " +
                     " WHERE ks.Kd_stat_edu='2'";
