42f54b8586a05919701f0ec3a02cf464d93e6c9d
jasoet87 <jasoet87@3a59f6d2-30a1-e0ef-d3eb-c9a0e4efb690>
2010-12-15 05:30:18 +0000

    Beres Tanggal 15 Desember

--- SIA-Terpadu/src/java/org/sadhar/sia/terpadu/nepdosen/NEPDosenDAOImpl.java
+++ SIA-Terpadu/src/java/org/sadhar/sia/terpadu/nepdosen/NEPDosenDAOImpl.java
@@ -5,7 +5,6 @@
 package org.sadhar.sia.terpadu.nepdosen;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 import org.sadhar.errhandler.ClassAntiNull;
